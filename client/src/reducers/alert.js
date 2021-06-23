6fe1e21ac5e8b52e2e06361364cdc9b224bcf022
Zhuoliang Pu <zhpu@ucsd.edu>
2021-06-23 22:10:11 +0800

    Add user authentication

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,20 @@
+import { SET_ALERT, REMOVE_ALERT } from "../actions/type";
+const initialState = [
+    // {
+    //     id: 1,
+    //     msg: 'Please log ing',
+    //     alertType: 'success'
+    // }
+];
+
+export default function(state = initialState, action ) {
+    const { type, payload} = action;
+    switch (type) {
+        case SET_ALERT:
+            return [...state, payload];
+        case REMOVE_ALERT:
+            return state.filter(alert => alert.id !== payload);
+        default:
+            return state;
+    }
+}
